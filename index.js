79c863c6f967276c68ccf6724c3a4f032cb9fb1d
viskoos <potniye@gmail.com>
2019-12-29 12:58:29 +0600

    feat: (index.js) import models and routes

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+require('./models/Product');
+
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/monodbs`);
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
 
 app.use(bodyParser.json());
+
+require('./routes/productRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
